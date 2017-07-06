8089d4015b25f4e609207823a5ceb21fb8a8537f
朱安邦的pro电脑 <broszhu@zhuanbangdeprodiannaodeMacBook-Pro.local>
2017-07-06 23:43:03 +0800

    MODI:完成图片懒加载

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -6,7 +6,7 @@
           <slider>
             <div v-for="item in recomments">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl" alt="pic">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl" alt="pic">
               </a>
             </div>
           </slider>
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img @load="loadImage" :src="item.imgurl" alt="pic" width="60" height="60">
+                <img v-lazy="item.imgurl" alt="pic" width="60" height="60">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
