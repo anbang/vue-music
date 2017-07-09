71521db078a86453aa155e53d10adbd779d1ccd0
朱安邦的pro电脑 <broszhu@zhuanbangdeprodiannaodeMacBook-Pro.local>
2017-07-09 15:33:13 +0800

    ADD:获取歌手数据

---
+++ src/api/singer.js
@@ -0,0 +1,23 @@
+import jsonp from '@/assets/js/jsonp.js'
+import { commonParams, options } from './config'
+
+export function getSingerList () {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    g_tk: 1773698800,
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    hostUin: 0,
+    format: 'jsonp',
+    inCharset: 'utf8',
+    outCharset: 'utf-8',
+    notice: 0,
+    platform: 'yqq',
+    needNewCode: 0
+  })
+
+  return jsonp(url, data, options)
+}
